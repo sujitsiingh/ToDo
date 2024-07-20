eab6aa73e860a7ac31a85d043096e3c134f2df86
Sujit Singh <114153271+sujitsiingh@users.noreply.github.com>
2024-07-21 03:40:03 +0530

    Final Commit

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 const dbConnect = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const todosRoutes = require('./routes/todoRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 // Set up Global configuration access
 require('dotenv').config();
@@ -16,15 +20,56 @@ app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Server is running...');
+app.get('/server', (req, res) => {
+  res.send('Server is running...');
 });
 
 // Import routes
+app.use('/api/user', authRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/todos', todosRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+// Swagger setup
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Todo API Backend',
+      version: '1.0.0',
+      description: 'Todo API Information',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+        description: 'Local server',
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Listening to the server
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`------- Server running on port ${PORT} --------`);
+  console.log(`------- Server running on port ${PORT} --------`);
 });
+
+module.exports = app;
